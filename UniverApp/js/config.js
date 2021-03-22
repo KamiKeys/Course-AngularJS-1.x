09ec2cb964e5c27ac201c53a36641b3ee778ed9d
Javier Amado <JavierAmado7@gmail.com>
2021-03-22 17:59:05 +0100

    Se agregan controladores, uso de ng-class y navegación con estilos en el menú

--- UniverApp/js/config.js
+++ UniverApp/js/config.js
@@ -1,13 +1,16 @@
 app.config(function($routeProvider){
 	// Cuando la ruta termina en / se redirige a home
 	$routeProvider.when('/',{
-		templateUrl: 'parciales/home.html'
+		templateUrl: 'parciales/home.html',
+		controller: 'inicioCtrl'
 	})
 	.when('/profesores',{
-		templateUrl: 'parciales/profesores.html'
+		templateUrl: 'parciales/profesores.html',
+		controller: 'profesoresCtrl'
 	})
 	.when('/alumnos',{
-		templateUrl: 'parciales/alumnos.html'
+		templateUrl: 'parciales/alumnos.html',
+		controller: 'alumnosCtrl'
 	})
 	.otherwise({ // Si hace cualquier otra cosa los redirige a /
 		redirectTo: '/'
