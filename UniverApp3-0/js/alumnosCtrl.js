1b3b7388f9e2228a6c858287262d8f204b5b9d56
Javier Amado <JavierAmado7@gmail.com>
2021-03-23 12:37:23 +0100

    Se crea una paginación de 5 con botones para avanzar y retroceder

--- UniverApp3-0/js/alumnosCtrl.js
+++ UniverApp3-0/js/alumnosCtrl.js
@@ -3,10 +3,23 @@ app.controller('alumnosCtrl', ['$scope','$http', function($scope,$http){
 	$scope.setActive("mAlumnos");
 
 	$scope.alumnos = {};
+	$scope.posicion = 5;
 
 	$http.get('php/servicios/alumnos.listado.php').then(function(data){
 
 		$scope.alumnos = data.data;
 	});
+
+	$scope.siguientes = function(){
+
+		if($scope.alumnos.length > $scope.posicion)
+			$scope.posicion += 5;
+	}
+
+	$scope.anteriores = function(){
+
+		if($scope.posicion > 5)
+			$scope.posicion -= 5;
+	}
 
 }]);
